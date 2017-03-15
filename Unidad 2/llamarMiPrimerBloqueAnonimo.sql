ac8f1f9f645e4027acd8e937671139a2c9e184e6
Juanganzo69 <juan1910@live.com.mx>
2017-03-15 16:41:19 -0600

    Procedimientos con numeros telefonicos

--- Unidad 2/llamarMiPrimerBloqueAnonimo.sql
+++ Unidad 2/llamarMiPrimerBloqueAnonimo.sql
@@ -2,9 +2,15 @@ DECLARE
 	ide				Employees.Employee_id%TYPE;
 	nombre_emp	 	Employees.first_name%TYPE;
 	apellido_emp 	Employees.last_name%TYPE;
-	salario_emp	 	Employees.salary%TYPE;
-	contrata_emp  	Employees.hire_date%TYPE;
+	salario_emp	 	VARCHAR2(20);
+	contrata_emp  	VARCHAR2(20);
+	numerito		VARCHAR2(100);
 BEGIN
         ide:=101;
-		consulta_emp(ide,nombre_emp,apellido_emp,salario_emp,contrata_emp);
+		consulta_emp(ide,nombre_emp,apellido_emp,salario_emp,contrata_emp,numerito);
+		DBMS_OUTPUT.PUT_LINE('Su ID es: '||ide||'.');
+		DBMS_OUTPUT.PUT_LINE('Su nombre es: '||nombre_emp||' '||apellido_emp||'.');
+		DBMS_OUTPUT.PUT_LINE('Sueldo: '||salario_emp||'.');
+		DBMS_OUTPUT.PUT_LINE('Su fecha de contratacion es: '||contrata_emp||'.');
+		DBMS_OUTPUT.PUT_LINE('Su fecha de contratacion es: '||numerito||'.');
 END;
--- Unidad 2/miPrimerBloqueAnonimo.sql
+++ Unidad 2/miPrimerBloqueAnonimo.sql
@@ -2,14 +2,25 @@ CREATE OR REPLACE PROCEDURE consulta_emp
 (	id 		 IN OUT		Employees.employee_id%TYPE,
 	nombre	 OUT	Employees.first_name%TYPE,
 	apellido OUT	Employees.last_name%TYPE,
-	salario	 OUT	Employees.salary%TYPE,
-	contrata OUT 	Employees.hire_date%TYPE
+	salario	 OUT	VARCHAR2,
+	contrata OUT 	VARCHAR2,
+	numero   IN OUT VARCHAR2
 )
 IS
 	BEGIN
-		SELECT employee_id, first_name, last_name, salary,hire_date
-		INTO   id, nombre, apellido, salario,contrata
+		SELECT employee_id, first_name, last_name, TO_CHAR(salary,'$999,999.99'),TO_CHAR(hire_date,'DD/MM/YYYY'),phone_number
+		INTO   id, nombre, apellido, salario,contrata,numero
 		FROM Employees
 		WHERE employee_id = id;
-		DBMS_OUTPUT.PUT_LINE(id||' '||nombre||' '||apellido||' '||salario||' '||contrata);
+		--condicional del formato de teléfono de estados unidos
+		IF LENGTH(numero) = 18 THEN
+		numero := REPLACE(numero,'.','');
+		numero := '+'||SUBSTR(numero,1,3)||'('||SUBSTR(numero,4,2)||')'||SUBSTR(numero
+			,6,4)||'-'||SUBSTR(numero,10);
+		--condicional del formato de México
+		ELSE
+		numero := REPLACE(numero,'.','');
+		numero := '+521('||SUBSTR(numero,1,3)||')'||SUBSTR(numero,4,3)||'-'||SUBSTR(numero
+			,7);
+		END IF;
 END;
