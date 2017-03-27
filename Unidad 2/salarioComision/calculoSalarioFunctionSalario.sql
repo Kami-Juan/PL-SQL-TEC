f10e9377a9cfdde4c4fbc1f8589e74ea0222277f
Juanganzo69 <juan1910@live.com.mx>
2017-03-27 08:27:09 -0600

    declare con la functionSalary

---
+++ Unidad 2/salarioComision/calculoSalarioFunctionSalario.sql
@@ -0,0 +1,22 @@
+DECLARE
+    CURSOR calculo_sal IS
+      SELECT e.first_name||' '||e.last_name, d.department_name, e.salary, funcSalary(e.salary,e.commission_pct)
+      FROM Employees e, Departments d
+      WHERE e.department_id = d.department_id AND d.department_name LIKE 'Sales';
+        nombre_completo VARCHAR(45);
+        nombre_depto    Departments.department_name%TYPE;
+        salario_emp     Employees.salary%TYPE;
+        salarioBefore     Employees.salary%TYPE;
+        BEGIN
+          OPEN calculo_sal;
+            LOOP
+                FETCH calculo_sal INTO nombre_completo, nombre_depto, salarioBefore, salario_emp;
+                    DBMS_OUTPUT.PUT_LINE('Su nombre es:'||nombre_completo||'.');
+                    DBMS_OUTPUT.PUT_LINE('El nombre de su departmaneto es:'||nombre_depto||'.');
+                    DBMS_OUTPUT.PUT_LINE('Su salario sin comision es:'||salarioBefore||'.');
+                    DBMS_OUTPUT.PUT_LINE('Su salario total es:'||salario_emp||'.');
+                    DBMS_OUTPUT.PUT_LINE('');
+                EXIT WHEN calculo_sal%NOTFOUND;
+            END LOOP;
+          CLOSE calculo_sal;
+        END;
