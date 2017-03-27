0a3b8aa027919320f9e59418807295d06f6a0d1d
Juanganzo69 <juan1910@live.com.mx>
2017-03-27 08:21:30 -0600

    llamar el procedure calcularIsr del paquete packIsr

---
+++ Unidad 2/paqueteCalcularIsr/llamarPackageIsr.sql
@@ -0,0 +1,8 @@
+DECLARE
+  nombre_empleado 	VARCHAR2(50);
+  salario_empleado 	Employees.salary%TYPE;
+  isrEmp 			NUMBER(20,4);
+  totalEmp 			NUMBER(20,4);
+BEGIN
+ packISR.calcular_ISR_procedure(nombre_empleado, salario_empleado, isrEmp,totalEmp);
+END;
